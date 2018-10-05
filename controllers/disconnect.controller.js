e7e27a1bf7ab003771b71c6024f550de4452b4bb
lorodin <38944437+lorodin@users.noreply.github.com>
2018-10-05 16:29:17 +1000

    Disconnect controller
    Create files for disconnect controller and disconnect controller tests.

---
+++ controllers/disconnect.controller.js
@@ -0,0 +1,11 @@
+class DisconnectController{
+    constructor(cacheService, dataBaseService, logger){
+        this._cache = cacheService;
+        this._db = dataBaseService;
+        this._logger = logger;
+    }
+
+    setAction(action, cb){
+        
+    }
+}
---
+++ test/disconnect.controller.tests.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+const DisconnectController = require('../controllers/disconnect.controller');
+const ActionModel = require('../models/action.model');
+
+describe('Disconnect controller', () => {
+    
+});
